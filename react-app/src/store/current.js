42bdaff04e22a0d5cf748bc4790bfae62f8df1ca
Jimson Ma <ma.jimson@gmail.com>
2021-08-05 20:52:46 -0700

    navbar styling

--- react-app/src/store/current.js
+++ react-app/src/store/current.js
@@ -1,8 +1,6 @@
 const SET_CURRENTWORKOUT = 'current/SET_CURRENTWORKOUT';
 const SET_CURRENTEXERCISE = 'current/SET_CURRENTEXERCISE';
 const SET_CURRENTACTIVITY = 'current/SET_CURRENTACTIVITY';
-const SET_WORKOUTICONS = 'current/SET_WORKOUTICONS';
-const SET_ACTIVITYICONS = 'current/SET_ACTIVITYICONS';
 
 const setCurrentWorkout = (workoutId) => ({
     type: SET_CURRENTWORKOUT,
@@ -19,15 +17,6 @@ const setCurrentActivity = (activityId) => ({
     payload: activityId
 })
 
-const setWorkoutIcons = (status) => ({
-    type: SET_WORKOUTICONS,
-    payload: status
-})
-
-// CONST setActivityIcons = (status) => ({
-
-// })
-
 export const getCurrentWorkout = (workoutId) => async (dispatch) => {
     dispatch(setCurrentWorkout(workoutId))
 }
@@ -38,10 +27,6 @@ export const getCurrentExercise = (exerciseId) => async (dispatch) => {
 
 export const getCurrentActivity = (activityId) => async (dispatch) => {
     dispatch(setCurrentActivity(activityId))
-}
-
-export const getWorkoutIcons = (status) => async (dispatch) => {
-    dispatch(setWorkoutIcons(status))
 }
 
 const initialState = {};
@@ -60,10 +45,6 @@ export default function reducer(state = initialState, action) {
         case SET_CURRENTACTIVITY:
             newState = { ...state }
             newState['currentActivityId'] = action.payload
-            return newState;
-        case SET_WORKOUTICONS:
-            newState = { ...state }
-            newState['workoutIcons'] = action.payload
             return newState;
         default:
             return state;
