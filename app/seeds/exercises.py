5c67bc1f7c97eaf22d8ed8e893648dd6eba544c6
Jimson Ma <ma.jimson@gmail.com>
2021-08-31 18:30:42 -0700

    changed seeders

--- app/seeds/exercises.py
+++ app/seeds/exercises.py
@@ -4,19 +4,19 @@ def seed_exercises():
     one = Exercise(
         exercise_name='Push-ups',
         calories_burned=7,
-        notes='{"blocks":[{"key":"t76n","text":"Test!","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}',
+        notes='{"blocks":[{"key":"t76n","text":"testing@@!","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}',
         user_id=1
     )
     two = Exercise(
         exercise_name='Sit-ups',
         calories_burned=3,
-        notes='{"blocks":[{"key":"t76n","text":"Test!!","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}',
+        notes='{"blocks":[{"key":"t76n","text":"testingzz@@!","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}',
         user_id=1
     )
     three = Exercise(
         exercise_name='One Mile Run',
         calories_burned=10,
-        notes='{"blocks":[{"key":"t76n","text":"Test!!!","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}',
+        notes='{"blocks":[{"key":"t76n","text":"testing??@@!","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}',
         user_id=1
     )
 
